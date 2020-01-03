15e8df5a5eedbb0187d80b54c25bdfb409e02a81
noinenil <noinenil@users.noreply.github.com>
2020-01-03 18:51:50 +0300

    Initial commit

---
+++ src/components/hello.vue
@@ -0,0 +1,48 @@
+<template>
+	<div class="hello">
+		<h1>{{myString}}</h1>
+		<p align="left">{{myText}}</p>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'hello',
+		created: function() {
+				var h1 = setInterval(this.writerLetters, 300);
+				setInterval(this.writerText, 100);
+				if (this.phrase.length < this.counter) {
+					clearInterval(h1);
+				}
+			
+		},
+		data: function() {
+			return ({
+				counter:0,
+				counterText:0,
+				phrase:'This is my first VUE component!\n',
+				text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident, deleniti?\n',
+				myString: '',
+				myText: '',
+			})
+
+		},
+		methods: {
+			writerLetters: function() {
+				if (this.phrase.length > this.counter) {
+				this.myString += this.phrase[this.counter]
+				this.counter++	
+				}		
+			},
+			writerText: function() {
+				if (this.text.length > this.counterText) {
+				this.myText += this.text[this.counterText]
+				this.counterText++
+				} else {
+					this.counterText = 0
+				}	
+			}
+		},
+	}
+
+</script>
